f63a2f2eb8d2e5a4ad2e6995aad7e2e2c58b6be8
mayankPrakashWavemaker <mayank.prakash@wavemaker.com>
2026-09-25 15:54:06 +0530

    Revert "feat(release): add release 12.0.2"
    This reverts commit 26cb22692504675b02c4729f4fd32ff157d372ef.

--- sidebar/sidebars/releaseNotesSidebar.js
+++ sidebar/sidebars/releaseNotesSidebar.js
@@ -1,44 +1,39 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarConfig} */
 export default [
   {
-    "type": "doc",
-    "id": "release-notes/index",
-    "label": "WaveMaker Releases"
+    type: 'doc',
+    id: 'release-notes/index',
+    label: 'WaveMaker Releases',
   },
   {
-    "type": "category",
-    "label": "Release - Version 12",
-    "collapsible": true,
-    "collapsed": true,
-    "items": [
+    type: 'category',
+    label: 'Release - Version 12',
+    collapsible: true,
+    collapsed: true,
+    items: [
       {
-        "type": "doc",
-        "id": "release-notes/release-version-12/the-announcement",
-        "label": "📣 The Announcement "
+        type: 'doc',
+        id: 'release-notes/release-version-12/the-announcement',
+        label: '📣 The Announcement ',
       },
       {
-        "type": "category",
-        "label": "12.0.x",
-        "collapsible": true,
-        "collapsed": true,
-        "items": [
+        type: 'category',
+        label: '12.0.x',
+        collapsible: true,
+        collapsed: true,
+        items: [
           {
-            "type": "doc",
-            "id": "release-notes/release-version-12/version-12-0-x/12.0.2",
-            "label": "12.0.2"
+            type: 'doc',
+            id: 'release-notes/release-version-12/version-12-0-x/12.0.1',
+            label: '12.0.1',
           },
           {
-            "type": "doc",
-            "id": "release-notes/release-version-12/version-12-0-x/12.0.1",
-            "label": "12.0.1"
+            type: 'doc',
+            id: 'release-notes/release-version-12/version-12-0-x/12.0.0',
+            label: '12.0.0',
           },
-          {
-            "type": "doc",
-            "id": "release-notes/release-version-12/version-12-0-x/12.0.0",
-            "label": "12.0.0"
-          }
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  },
 ];
